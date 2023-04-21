daf545c482fb45c02c315f79d53e98f1c424a9be
cesar <cr8297408@gmail.com>
2023-04-20 20:07:11 -0500

    ready providers migration

---
+++ src/controllers/Bulks/providers/functions/index.ts
@@ -0,0 +1,17 @@
+export interface providerExcel {
+  id?: string;
+  name: string;
+  name2: string;
+  lastName: string;
+  lastName2: string;
+  TypeDoc: string;
+  DNI: string;
+  phone: string;
+  phone2: string;
+  phone3: string;
+  country: string;
+  state: string;
+  city: string;
+  address: string;
+  email: string;
+}
--- src/routes/Bulks/index.ts
+++ src/routes/Bulks/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {BpExample} from '../../controllers/Bulks'
+import {BpExample, BpSupplier} from '../../controllers/Bulks'
 import { ValidateAndDecryptTokenBase } from "../../middlewares";
 
 const bulkRouter = Router();
 
 bulkRouter.post("/BP", [ValidateAndDecryptTokenBase], BpExample)
+bulkRouter.post('/suppliers', [ValidateAndDecryptTokenBase], BpSupplier);
 
 
 export default bulkRouter
