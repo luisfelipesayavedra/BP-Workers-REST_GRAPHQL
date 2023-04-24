cb37e671b42cc77354f4bc1258e20e23a580fead
cesar <cr8297408@gmail.com>
2023-04-24 10:43:28 -0500

    ready migration note credits

--- src/controllers/Bulks/index.ts
+++ src/controllers/Bulks/index.ts
@@ -1,2 +1,4 @@
 export * from './example'
 export * from './providers'
+
+export * from './noteCredits'
--- src/routes/Bulks/index.ts
+++ src/routes/Bulks/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import {BpExample, BpSupplier} from '../../controllers/Bulks'
+import {
+  BpExample,
+  BpSupplier,
+  BpUser,
+  BpNoteCredit,
+} from '../../controllers/Bulks';
 import { ValidateAndDecryptTokenBase } from "../../middlewares";
 
 const bulkRouter = Router();
 
 bulkRouter.post("/BP", [ValidateAndDecryptTokenBase], BpExample)
 bulkRouter.post('/suppliers', [ValidateAndDecryptTokenBase], BpSupplier);
+bulkRouter.post('/users', [ValidateAndDecryptTokenBase], BpUser);
+bulkRouter.post('/note-credits', [ValidateAndDecryptTokenBase], BpNoteCredit);
 
 
 export default bulkRouter
